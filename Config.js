bcfa8400f485a7a4cd569f8e6921d50dbe6e0b79
raja-mobii26 <raaj96410@gmail.com>
2025-06-24 22:14:43 +0500

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,117 @@
+import { watchFile, unwatchFile } from 'fs'
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+const defaultOwner = '923113293808';
+
+
+// Check for the OWNERS environment variable; if not found, use the default
+const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '923113293808' ; // put your number here
+
+const ownerlist = ownervb.split(';');
+
+global.owner = [];
+for (let i = 0; i < ownerlist.length; i++) {
+    global.owner.push([ownerlist[i], true]);
+}
+//
+global.botname = process.env.BOTNAME || 'TOHID-AI';
+global.pairingNumber = process.env.BOT_NUMBER || '923113293808' ;  // put your number here
+global.SESSION_ID = process.env.SESSION_ID || '8t66bI6TDe37SX5DYwUoHJZlurVkyFPjTfpDQUvjI' ;  // put your session id here
+
+global.mods = []
+global.prems = []
+global.allowed = ['917849917350', '918930358452']
+global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
+global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
+global.keysxteammm = [
+  '29d4b59a4aa687ca',
+  '5LTV57azwaid7dXfz5fzJu',
+  'cb15ed422c71a2fb',
+  '5bd33b276d41d6b4',
+  'HIRO',
+  'kurrxd09',
+  'ebb6251cc00f9c63',
+]
+global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
+global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
+global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
+global.lolkeysapi = ['GataDios']
+
+global.canal = 'https://whatsapp.com/channel/0029VaGyP933bbVC7G0x0i2T'
+
+
+global.APIs = {
+  // API Prefix
+  // name: 'https://website'
+  xteam: 'https://api.xteam.xyz',
+  dzx: 'https://api.dhamzxploit.my.id',
+  lol: 'https://api.lolhuman.xyz',
+  violetics: 'https://violetics.pw',
+  neoxr: 'https://api.neoxr.my.id',
+  zenzapis: 'https://zenzapis.xyz',
+  akuari: 'https://api.akuari.my.id',
+  akuari2: 'https://apimu.my.id',
+  nrtm: 'https://fg-nrtm.ddns.net',
+  bg: 'http://bochil.ddns.net',
+  fgmods: 'https://api.fgmods.xyz',
+}
+global.APIKeys = {
+  // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
+  'https://api.neoxr.my.id': `${keysneoxr}`,
+  'https://violetics.pw': 'beta',
+  'https://zenzapis.xyz': `${keysxxx}`,
+  'https://api.fgmods.xyz': 'm2XBbNvz',
+}
+
+
+// Bot Images 
+global.imagen1 = fs.readFileSync("./assets/tohid.jpg")
+global.imagen2 = fs.readFileSync("./assets/A.jpg")
+//💌------------------------------------------💌
+
+// Randome
+global.tohidImg = [imagen1, imagen2]
+
+
+
+
+// Sticker WM
+global.premium = 'true'
+global.packname = 'TOHID-AI'
+global.author = 'Tohidkhan6332'
+global.menuvid = 'https://i.imgur.com/2Sp3cqD.mp4'
+global.igfg = ' Follow on Instagram\nhttps://www.instagram.com/Tohidkhan6332'
+global.dygp = 'https://whatsapp.com/channel/0029VaGyP933bbVC7G0x0i2T'
+global.fgsc = 'https://github.com/Tohidkhan6332/TOHID-AI'
+global.fgyt = 'https://youtube.com/@Tohidkhan_6332'
+global.fgpyp = 'https://GitHub.com/Tohidkhan6332'
+global.fglog = 'https://i.imgur.com/WnKjrJt.jpeg'
+global.thumb = fs.readFileSync('./assets/tohid.jpg')
+
+//Reactions
+global.wait = '*`⏰ 𝙷𝚘𝚕𝚍 𝙾𝚗 𝙿𝚛𝚘𝚌𝚎𝚜𝚜𝚒𝚗𝚐...`*'
+global.imgs = '*🖼️ _𝙶𝙴𝚃𝚃𝙸𝙽𝙶 𝚈𝙾𝚄𝚁 ɪᴍᴀɢᴇs 𝚆𝙰𝙸𝚃..._*\n*▰▰▰▱▱▱▱▱*'
+global.rwait = '⏳'
+global.dmoji = '🤭'
+global.done = '✅'
+global.error = '❌' 
+global.xmoji = '🌀' 
+global.multiplier = 69 
+global.maxwarn = '2' 
+global.eror = '```404 error```'
+//💌------------------------------------------💌
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
